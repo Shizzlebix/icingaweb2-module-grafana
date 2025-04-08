8df916801e5143b89de33d491d41bd76c40b4cbe
Shizzlebix <gregor-@gmx.de>
2025-04-08 12:24:40 +0200

    Update Timeranges.php
    update to new one

--- library/Grafana/Helpers/Timeranges.php
+++ library/Grafana/Helpers/Timeranges.php
@@ -5,14 +5,21 @@
  * Date: 19.02.2018
  * Time: 19:05
  */
+
 namespace Icinga\Module\Grafana\Helpers;
 
 use Icinga\Application\Icinga;
+use Icinga\Application\Modules\Module;
+use Icinga\Module\Grafana\ProvidedHook\Icingadb\IcingadbSupport;
 
 
 class Timeranges
 {
-     static $timeRanges = array(
+    private $urlparams;
+    private $link;
+    private $view;
+
+    static $timeRanges = array(
         'Minutes' => array(
             '5m' => '5 minutes',
             '15m' => '15 minutes',
@@ -27,13 +34,13 @@ class Timeranges
             '12h' => '12 hours',
             '24h' => '24 hours'
         ),
-        'Days' => array (
+        'Days' => array(
             '2d' => '2 days',
             '7d' => '7 days',
             '14d' => '14 days',
             '30d' => '30 days',
         ),
-        'Months' => array (
+        'Months' => array(
             '2M' => '2 month',
             '6M' => '6 months',
             '9M' => '9 months'
@@ -52,21 +59,22 @@ class Timeranges
         )
     );
 
-
     public function __construct(array $array = array(), $link = "")
     {
-        $this->array = $array;
+        $this->urlparams = $array;
         $this->link = $link;
+
+        $this->view = Icinga::app()->getViewRenderer()->view;
     }
+
     private function getTimerangeLink($rangeName, $timeRange)
     {
-        $this->view = Icinga::app()->getViewRenderer()->view;
-        $this->array['timerange'] = $timeRange;
+        $this->urlparams['timerange'] = $timeRange;
 
         return $this->view->qlink(
             $rangeName,
             $this->link,
-            $this->array,
+            $this->urlparams,
             array(
                 'class' => 'action-link',
                 'data-base-target' => '_self',
@@ -75,6 +83,7 @@ class Timeranges
         );
     }
 
+
     protected function isValidTimeStamp($timestamp)
     {
         return ((string) (int) $timestamp === $timestamp)
@@ -82,15 +91,22 @@ class Timeranges
             && ($timestamp >= ~PHP_INT_MAX);
     }
 
-    private function buildTimrangeMenu($timerange = "", $timerangeto = "")
+    private function buildTimerangeMenu($timerange = "", $timerangeto = "")
     {
+        $url = 'grafana/icingadbdashboard?';
+
+        $clockIcon = $this->view->qlink($timerange, 'dashboard/new-dashlet',
+            ['url' => $url . http_build_query($this->urlparams, '', '&', PHP_QUERY_RFC3986)],
+            ['icon' => 'clock', 'title' => 'Add graph to dashboard']);
+
         $menu = '<table class="grafana-table"><tr>';
-        $menu .= '<td><div class="grafana-icon"><div class="grafana-clock"></div></div></td>';
+        $menu .= '<td>' . $clockIcon . '</td>';
         foreach (self::$timeRanges as $key => $mainValue) {
             $menu .= '<td><ul class="grafana-menu-navigation"><a class="main" href="#">' . $key . '</a>';
             $counter = 1;
             foreach ($mainValue as $subkey => $value) {
-                $menu .= '<li class="grafana-menu-n'. $counter .'">' . $this->getTimerangeLink($value, $subkey) . '</li>';
+                $menu .= '<li class="grafana-menu-n' . $counter . '">' . $this->getTimerangeLink($value,
+                        $subkey) . '</li>';
                 $counter++;
             }
             $menu .= '</ul></td>';
@@ -111,49 +127,16 @@ class Timeranges
             $timerangeto = $d->format("Y-m-d H:i:s");
         }
 
-        unset($this->array['timerange']);
-        $form_link = $this->view->url($this->link, $this->array);
-        $menu .= '<td>
-                    <form method="get" class="grafana-module-tr-form" action="'.$form_link.'">
-                        <input type="text" value="'.$timerange.'" placeholder="from" name="tr-from" />
-                        <input type="text" value="'.$timerangeto.'" placeholder="to" name="tr-to" />
-                        <a href="'.$form_link.'" data-base-target="_self" class="action-link grafana-module-tr-apply">Apply</a>
-                    </form>
-                  </td>';
-        $menu .= '<script type="text/javascript">
-$( document ).ready(function() {
-    $("a.grafana-module-tr-apply").click(function() {
-        var old_href = $(this).attr("href");
-        var tr_from = $("input[name=tr-from]").val();
-        var tr_to = $("input[name=tr-to]").val();
-        
-        var d_tr_from = new Date(tr_from);
-        var d_tr_to = new Date(tr_to);
-                
-        if(d_tr_from != "Invalid Date") {
-            tr_from = d_tr_from.getTime();
-        }
-        if(d_tr_to != "Invalid Date") {
-            tr_to = d_tr_to.getTime();
-        }
-        
-        var new_href = old_href + "&tr-from=" + encodeURIComponent(tr_from) + "&tr-to=" + encodeURIComponent(tr_to);
-        $(this).attr("href", new_href);
-    });    
-});
-</script>';
         $menu .= '</tr></table>';
-
         return $menu;
     }
 
     public function getTimerangeMenu($timerange = "", $timerangeto = "")
     {
-        return $this->buildTimrangeMenu($timerange, $timerangeto);
+        return $this->buildTimerangeMenu($timerange, $timerangeto);
     }
 
     public static function getTimeranges()
     {
-        return call_user_func_array('array_merge',self::$timeRanges);
+        return call_user_func_array('array_merge', array_values(self::$timeRanges));
     }
-}
